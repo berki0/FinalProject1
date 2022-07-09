af1ff8460e453781dc26ee3bea5d6ef452e060a3
berki0 <berki9@icloud.com>
2022-07-09 20:17:30 +0300

    нов начин

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,107 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void startMenu() {
-        Scanner scanner=new Scanner(System.in);
+    public static void startMenu(String[]inf) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Please choose what to do (0 - Exit; 1 - List all items; 2 - Add new delivery; 3 - List deliveries for time period)");
-        int choice= scanner.nextInt();
-        switch (choice){
-            case 0:break;
-            case 1:
-                listAllItems();break;
-            case 2:
-                addNewDelivery();break;
-            case 3:
-                listDeliveriesForTimePeriod();break;
+        String choice = scanner.nextLine();
+        switch (choice) {
+            case "0":
+                break;
+            case "1":
+                listAllItems(inf);
+                break;
+            case "2":
+
+                int allInf = 0;
+                if (inf[0] == null) {
+                    String[]allInformation=new String[171];
+                    System.out.println("Enter product name: ");
+                    allInformation[0] = scanner.next();
+                    allInf += 1;
+                    System.out.println("Enter expiry date:");
+                    allInformation[allInf] = scanner.next();
+                    allInf += 1;
+                    System.out.println("Enter entry date:: ");
+                    allInformation[allInf] = scanner.next();
+                    allInf += 1;
+                    System.out.println("Enter manufacturer: ");
+                    allInformation[allInf] = scanner.next();
+                    allInf += 1;
+                    System.out.println("Enter unit: ");
+                    allInformation[allInf] = scanner.next();
+                    allInf += 1;
+                    System.out.println("Enter available stock: ");
+                    allInf += 1;
+                    allInformation[allInf] = scanner.next();
+                    System.out.println("Enter comment (optional)");
+                    allInf += 1;
+                    allInformation[allInf] = scanner.next();
+                    allInf += 1;
+                    System.out.println("Product was added successfully!");
+
+                    startMenu(allInformation);
+                } else {
+                    allInf += 9;
+                    String[]allInformation=new String[171];
+                    System.out.println("Enter product name: ");
+                    allInformation[0] = scanner.next();
+                    allInf += 1;
+                    System.out.println("Enter expiry date:");
+                    allInformation[allInf] = scanner.next();
+                    allInf += 1;
+                    System.out.println("Enter entry date:: ");
+                    allInformation[allInf] = scanner.next();
+                    allInf += 1;
+                    System.out.println("Enter manufacturer: ");
+                    allInformation[allInf] = scanner.next();
+                    allInf += 1;
+                    System.out.println("Enter unit: ");
+                    allInformation[allInf] = scanner.next();
+                    allInf += 1;
+                    System.out.println("Enter available stock: ");
+                    allInf += 1;
+                    allInformation[allInf] = scanner.next();
+                    System.out.println("Enter comment (optional)");
+                    allInf += 1;
+                    allInformation[allInf] = scanner.next();
+                    allInf += 1;
+                    System.out.println("Product was added successfully!");
+                    startMenu(allInformation);
+                }
+                break;
+            case "3":
+                listDeliveriesForTimePeriod();
+                break;
             default:
                 System.out.println("Invalid Selection");
                 System.out.println("Please select again (1-3)");
-                startMenu();
+                startMenu(inf);
         }
     }
-    public static void listAllItems(){
-        System.out.println("test");
+    public static void listAllItems(String[]information) {
+      if (information[0]!=null) {
+    System.out.println("Product name: " + information[0]);
+    System.out.println("Expiry date: " + information[1]);
+    System.out.println("Manufacturer: " + information[2]);
+    System.out.println("Unit: " + information[3]);
+    System.out.println("Stock: " + information[4]);
+    System.out.println("Position: " + information[5]);
+    System.out.println("Available items at shelf: " + information[6]);
+    System.out.println("Comment: " + information[7]);
+    startMenu(information);
+       }else{
+          System.out.println("Please enter products");
+          startMenu(information);
+      }
     }
-    public static void addNewDelivery(){
-        Scanner scanner=new Scanner(System.in);
-        String nameProduct[]=new String[19];
-        String expiryDate[]=new String[19];
-        String dateОfАrrival[]=new String[19];
-        String manufacturer[]=new String[19];
-        int unit[]=new int[19];
-        int availableQuantity[]=new int[19];
-        String location[]=new String[19];
-        String availabeItemAtShelf[]=new String[19];
-        String coment[]=new String[19];
-        int numberProduct=0;
-        System.out.println("Enter product name: ");
-        nameProduct[numberProduct]=scanner.next();
-        System.out.println("Enter expiry date:");
-        expiryDate[numberProduct]= scanner.next();
-        System.out.println("Enter entry date:: ");
-        dateОfАrrival[numberProduct]= scanner.next();
-        System.out.println("Enter manufacturer: ");
-        manufacturer[numberProduct]= scanner.next();
-        System.out.println("Enter unit: ");
-        unit[numberProduct]= scanner.nextInt();
-        System.out.println("Enter available stock: ");
-        availableQuantity[numberProduct]= scanner.nextInt();
-        System.out.println("Enter comment (optional)");
-        coment[numberProduct]= scanner.next();
-        System.out.println("Product was added successfully!");
-        numberProduct+=1;
-        startMenu();
-    }
-    public static void listDeliveriesForTimePeriod(){
+    public static void listDeliveriesForTimePeriod() {
         System.out.println("test");
     }
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        startMenu();
+        Scanner scanner = new Scanner(System.in);
+        String allInformation[] = new String[171];
+        startMenu(allInformation);
+
     }
 }
