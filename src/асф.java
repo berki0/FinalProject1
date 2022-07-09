e58076b7769c1ecaed9c2ad6391a3c9442ca9fd5
berki0 <berki9@icloud.com>
2022-07-09 20:18:56 +0300

    нов начин

---
+++ src/асф.java
@@ -0,0 +1,77 @@
+public class асф {
+    public static void main(String[] args) {
+        // String choice="0";
+        //    String[]listAllItems=new String[171];
+        //        switch (choice) {
+        //        case "0":
+        //            break;
+        //        case "1":
+        //            listAllItems(allInformation);
+        //            break;
+        //        case "2":
+        //            int allInf = 0;
+        //            if (allInformation[0] == null) {
+        //                System.out.println();
+        //                System.out.println("Enter product name: ");
+        //                allInformation[0] = scanner.next();
+        //                allInf += 1;
+        //                System.out.println("Enter expiry date:");
+        //                allInformation[allInf] = scanner.next();
+        //                allInf += 1;
+        //                System.out.println("Enter entry date:: ");
+        //                allInformation[allInf] = scanner.next();
+        //                allInf += 1;
+        //                System.out.println("Enter manufacturer: ");
+        //                allInformation[allInf] = scanner.next();
+        //                allInf += 1;
+        //                System.out.println("Enter unit: ");
+        //                allInformation[allInf] = scanner.next();
+        //                allInf += 1;
+        //                System.out.println("Enter available stock: ");
+        //                allInf += 1;
+        //                allInformation[allInf] = scanner.next();
+        //                System.out.println("Enter comment (optional)");
+        //                allInf += 1;
+        //                allInformation[allInf] = scanner.next();
+        //                allInf += 1;
+        //                System.out.println("Product was added successfully!");
+        //                startMenu(allInformation);
+        //            } else {
+        //                allInf += 9;
+        //                System.out.println("Enter product name: ");
+        //                allInformation[0] = scanner.next();
+        //                allInf += 1;
+        //                System.out.println("Enter expiry date:");
+        //                allInformation[allInf] = scanner.next();
+        //                allInf += 1;
+        //                System.out.println("Enter entry date:: ");
+        //                allInformation[allInf] = scanner.next();
+        //                allInf += 1;
+        //                System.out.println("Enter manufacturer: ");
+        //                allInformation[allInf] = scanner.next();
+        //                allInf += 1;
+        //                System.out.println("Enter unit: ");
+        //                allInformation[allInf] = scanner.next();
+        //                allInf += 1;
+        //                System.out.println("Enter available stock: ");
+        //                allInf += 1;
+        //                allInformation[allInf] = scanner.next();
+        //                System.out.println("Enter comment (optional)");
+        //                allInf += 1;
+        //                allInformation[allInf] = scanner.next();
+        //                allInf += 1;
+        //                System.out.println("Product was added successfully!");
+        //                startMenu(allInformation);
+        //            }
+        //            break;
+        //        case "3":
+        //            listDeliveriesForTimePeriod();
+        //            break;
+        //        default:
+        //            System.out.println("Invalid Selection");
+        //            System.out.println("Please select again (1-3)");
+        //            startMenu(allInformation);
+    }
+
+
+}
