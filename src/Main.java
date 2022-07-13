6c104f85f52575386ed036a18673854f94ec67da
berki0 <berki9@icloud.com>
2022-07-13 18:55:19 +0300

    Основа на програмата е готова

--- src/Main.java
+++ src/Main.java
@@ -1,231 +1,21 @@
 import java.util.Scanner;
 public class Main {
-    
-    public static void startMenu(String[][]inf) {
+
+    public static void startMenu(String[][] allinf,int j) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please choose what to do (0 - Exit; 1 - List all items; 2 - Add new delivery; 3 - List deliveries for time period)");
         String choice = scanner.nextLine();
+        int q = 0;// втори масив
+
         switch (choice) {
-            case "0":
+            case "0"://exit
                 break;
             case "1":
-                listAllItems(inf); break;
+                listAllItems(allinf,j);
+                break;
             case "2":
-                int o = 0;
-                if (inf[0][0] == null) {
-                    System.out.println("Enter product name: ");
-                    inf[0][o] = scanner.next();//product name
-                    o += 1;
-                    System.out.println("Enter expiry date:");
-                    inf[0][o] = scanner.next();//expiry date
-                    o += 1;
-                    System.out.println("Enter entry date:: ");
-                    inf[0][o] = scanner.next();//entry date
-                    o += 1;
-                    System.out.println("Enter manufacturer: ");
-                    inf[0][o] = scanner.next();//manufacturer
-                    o += 1;
-                    System.out.println("Enter unit: ");
-                    inf[0][o] = scanner.next();//units
-                    o += 1;
-                    System.out.println("Enter available stock: ");
-                    inf[0][o] = scanner.next();//available stocks
-                    o += 1;
-                    System.out.println("Enter comment (optional)");
-                    inf[0][o] = scanner.next();//comments
-                    System.out.println("Product was added successfully!");
-                    System.out.println("Enter size A: ");
-                    int sizeA= scanner.nextInt();
-                    System.out.println("Enter size B: ");
-                    int sizeB= scanner.nextInt();
-                    System.out.println("Enter size C: ");
-                    int sizeC= scanner.nextInt();
-                    int sumSize=sizeA*sizeC*sizeB;
-                    System.out.println("Size to m3: "+sumSize );
-                    System.out.println("Product was added successfully!");
-                    int position= 1;
-                    position+=1;
-                    inf[0][7]=String.valueOf(position);
-                    startMenu(inf);
-                }else if (inf[1][0]==null ){
-                    int j=1;
-                    System.out.println("Enter product name: ");
-                inf[j][o] = scanner.next();
-                o += 1;
-                System.out.println("Enter expiry date:");
-                inf[j][o] = scanner.next();
-                o += 1;
-                System.out.println("Enter entry date:: ");
-                inf[j][o] = scanner.next();
-                o += 1;
-                System.out.println("Enter manufacturer: ");
-                inf[j][o] = scanner.next();
-                o += 1;
-                System.out.println("Enter unit: ");
-                inf[j][o] = scanner.next();
-                o += 1;
-                System.out.println("Enter available stock: ");
-                inf[j][o] = scanner.next();
-                o += 1;
-                System.out.println("Enter comment (optional)");
-                inf[j][o] = scanner.next();
-                System.out.println("Enter size A: ");
-                int sizeA= scanner.nextInt();
-                System.out.println("Enter size B: ");
-                int sizeB= scanner.nextInt();
-                System.out.println("Enter size C: ");
-                int sizeC= scanner.nextInt();
-                int sumSize=sizeA*sizeC*sizeB;
-                int position= 1;
-                position+=1;
-                inf[j][7]=String.valueOf(position);
-                System.out.println("Size to m3: "+sumSize );
-                System.out.println("Product was added successfully!");
-                startMenu(inf);
-            }else if (inf[2][0]==null ){
-                    int j=2;
-                    o=0;
-                    System.out.println("Enter product name: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter expiry date:");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter entry date:: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter manufacturer: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter unit: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter available stock: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter comment (optional)");
-                    inf[j][o] = scanner.next();
-                    System.out.println("Enter size A: ");
-                    int sizeA= scanner.nextInt();
-                    System.out.println("Enter size B: ");
-                    int sizeB= scanner.nextInt();
-                    System.out.println("Enter size C: ");
-                    int sizeC= scanner.nextInt();
-                    int sumSize=sizeA*sizeC*sizeB;
-                    int position= 1;
-                    position+=1;
-                    inf[j][7]=String.valueOf(position);
-                    System.out.println("Size to m3: "+sumSize );
-                    System.out.println("Product was added successfully!");
-                    startMenu(inf);
-                } else if (inf[3][0]==null ){
-                    int j=3;
-                    System.out.println("Enter product name: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter expiry date:");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter entry date:: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter manufacturer: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter unit: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter available stock: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter comment (optional)");
-                    inf[j][o] = scanner.next();
-                    System.out.println("Enter size A: ");
-                    int sizeA= scanner.nextInt();
-                    System.out.println("Enter size B: ");
-                    int sizeB= scanner.nextInt();
-                    System.out.println("Enter size C: ");
-                    int sizeC= scanner.nextInt();
-                    int sumSize=sizeA*sizeC*sizeB;
-                    int position= 1;
-                    position+=1;
-                    inf[j][7]=String.valueOf(position);
-                    System.out.println("Size to m3: "+sumSize );
-                    System.out.println("Product was added successfully!");
-                    startMenu(inf);
-                }else if (inf[4][0]==null ){
-                    int j=4;
-                    System.out.println("Enter product name: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter expiry date:");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter entry date:: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter manufacturer: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter unit: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter available stock: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter comment (optional)");
-                    inf[j][o] = scanner.next();
-                    System.out.println("Enter size A: ");
-                    int sizeA= scanner.nextInt();
-                    System.out.println("Enter size B: ");
-                    int sizeB= scanner.nextInt();
-                    System.out.println("Enter size C: ");
-                    int sizeC= scanner.nextInt();
-                    int sumSize=sizeA*sizeC*sizeB;
-                    int position= 1;
-                    position+=1;
-                    inf[j][7]=String.valueOf(position);
-                    System.out.println("Size to m3: "+sumSize );
-                    System.out.println("Product was added successfully!");
-                    startMenu(inf);
-                }else {
-                    int j=5;
-                    o=0;
-                    System.out.println("Enter product name: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter expiry date:");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter entry date:: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter manufacturer: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter unit: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter available stock: ");
-                    inf[j][o] = scanner.next();
-                    o += 1;
-                    System.out.println("Enter comment (optional)");
-                    o += 1;
-                    inf[j][o] = scanner.next();
-                    System.out.println("Enter size A: ");
-                    int sizeA= scanner.nextInt();
-                    System.out.println("Enter size B: ");
-                    int sizeB= scanner.nextInt();
-                    System.out.println("Enter size C: ");
-                    int sizeC= scanner.nextInt();
-                    int sumSize=sizeA*sizeC*sizeB;
-                    int position= 1;
-                    position+=1;
-                    inf[j][7]=String.valueOf(position);
-                    System.out.println("Size to m3: "+sumSize );
-                    System.out.println("Product was added successfully!");
-                    startMenu(inf);
-                }
+                j++;
+                addNewItem(allinf, j, q);
                 break;
             case "3":
                 listDeliveriesForTimePeriod();
@@ -233,103 +23,92 @@ public class Main {
             default:
                 System.out.println("Invalid Selection");
                 System.out.println("Please select again (1-3)");
-                startMenu(inf);
+                startMenu(allinf,j);
         }
     }
-    public static void listAllItems(String[][]information) {
-      if (information[0][0]!=null) {
-          int a=0;
-          int j=0;
-    System.out.println("Product name: " + information[j][a]);
-          a+=1;
-    System.out.println("Expiry date: " + information[j][a]);
-          a+=1;
-    System.out.println("Manufacturer: " + information[j][a]);
-          a+=1;
-    System.out.println("Unit: " + information[j][a]);
-          a+=1;
-    System.out.println("Stock: " + information[j][a]);
-          a+=1;
-    System.out.println("Available items at shelf: " + information[j][a]);
-          a+=1;
-    System.out.println("Comment: " + information[j][a]);
-          a+=1;
-          System.out.println("Position: A-" + information[j][a]);
-          System.out.println();
-          if (information[1][0]!=null) {
-              a=0;
-              j=1;
-              System.out.println("Product name: " + information[j][a]);
-              a += 1;
-              System.out.println("Expiry date: " + information[j][a]);
-              a += 1;
-              System.out.println("Manufacturer: " + information[j][a]);
-              a += 1;
-              System.out.println("Unit: " + information[j][a]);
-              a += 1;
-              System.out.println("Stock: " + information[j][a]);
-              a += 1;
-              System.out.println("Available items at shelf: " + information[j][a]);
-              a += 1;
-              System.out.println("Comment: " + information[j][a]);
-              a += 1;
-              System.out.println("Position: A-" + information[j][a]);
-              System.out.println();
-              if (information [2][0]!= null) {
-                  a=0;
-                  j=2;
-                  System.out.println("Product name: " + information[j][a]);
-                  a += 1;
-                  System.out.println("Expiry date: " + information[j][a]);
-                  a += 1;
-                  System.out.println("Manufacturer: " + information[j][a]);
-                  a += 1;
-                  System.out.println("Unit: " + information[j][a]);
-                  a += 1;
-                  System.out.println("Stock: " + information[j][a]);
-                  a += 1;
-                  System.out.println("Available items at shelf: " + information[j][a]);
-                  a += 1;
-                  System.out.println("Comment: " + information[j][a]);
-                  a += 1;
-                  System.out.println("Position: A-" + information[j][a]);
-                  System.out.println();
-                  if (information[3][0] != null) {
-                      a=0;
-                      j=3;
-                      System.out.println("Product name: " + information[j][a]);
-                      a += 1;
-                      System.out.println("Expiry date: " + information[j][a]);
-                      a += 1;
-                      System.out.println("Manufacturer: " + information[j][a]);
-                      a += 1;
-                      System.out.println("Unit: " + information[j][a]);
-                      a += 1;
-                      System.out.println("Stock: " + information[j][a]);
-                      a += 1;
-                      System.out.println("Available items at shelf: " + information[j][a]);
-                      a += 1;
-                      System.out.println("Comment: " + information[j][a]);
-                      a += 1;
-                      System.out.println("Position: A-" + information[j][a]);
-                      System.out.println();
-                  }
-              }
-          }
-    startMenu(information);
-       }else{
-          System.out.println("Please enter products");
-          startMenu(information);
-      }
+    public static String[][] addNewItem(String[][] allInf, int j, int q) {
+        Scanner scanner = new Scanner(System.in);
+            while (allInf[j][q] == null){
+                q=0;
+                    System.out.println("Enter product name: ");
+                    allInf[j][q] = scanner.nextLine();//product name
+                    q += 1;
+                    System.out.println("Enter expiry date:");
+                    allInf[j][q] = scanner.nextLine();//expiry date
+                    q += 1;
+                    System.out.println("Enter entry date:: ");
+                    allInf[j][q] = scanner.nextLine();//entry date
+                    q += 1;
+                    System.out.println("Enter manufacturer: ");
+                    allInf[j][q] = scanner.nextLine();//manufacturer
+                    q += 1;
+                    System.out.println("Enter unit: ");
+                    allInf[j][q] = scanner.nextLine();//units
+                    q += 1;
+                    System.out.println("Enter available stock: ");
+                    allInf[j][q] = scanner.nextLine();//available stocks
+                    q += 1;
+                    System.out.println("Enter comment (optional)");
+                    allInf[j][q] = scanner.nextLine();//comments
+                    System.out.println("Product was added successfully!");
+                    System.out.println("Enter size A (as meters 60sm = 0,6 sm): ");
+                    double sizeA = scanner.nextDouble();
+                    System.out.println("Enter size B (as meters 60sm = 0,6 sm): ");
+                    double sizeB = scanner.nextDouble();
+                    System.out.println("Enter size C (as meters 60sm = 0,6 sm): ");
+                    double sizeC = scanner.nextDouble();
+                    double sumSize = sizeA * sizeC * sizeB*Integer.valueOf(allInf[j][6]);
+                    System.out.println("Size to m3: " + sumSize);
+                    System.out.println("Product was added successfully!");
+                    double position = 1.0;
+                    position += 1;
+                    allInf[j][7] = String.valueOf(position);
+                System.out.println();
+                break;
+        }
+        startMenu(allInf,j);
+            return allInf;
     }
-    public static void listDeliveriesForTimePeriod() {
-        System.out.println("test");
+        public static void listAllItems (String[][]allInf,int j){
+            if (allInf[0][0]==null){
+                System.out.println("Please enter Products !");
+                startMenu(allInf,j);
+            }
+            j=0;
+        while (allInf[j][0] != null){
+            int q=0;
+                System.out.println("Product name: " + allInf[j][q]);
+                q += 1;
+                System.out.println("Expiry date: " + allInf[j][q]);
+                q += 1;
+                System.out.println("Manufacturer: " + allInf[j][q]);
+                q += 1;
+                System.out.println("Unit: " + allInf[j][q]);
+                q += 1;
+                System.out.println("Stock: " + allInf[j][q]);
+                q += 1;
+                System.out.println("Available items at shelf: " + allInf[j][q]);
+                q += 1;
+                System.out.println("Comment: " + allInf[j][q]);
+                q += 1;
+                System.out.println("Position: A-" + allInf[j][q]);
+            System.out.println();
+                j++;
+                System.out.println();
+
+        }
+        startMenu(allInf,j);
+        }
+        public static void listDeliveriesForTimePeriod () {
+            System.out.println("test");
+        }
+        public static void main (String[]args){
+            String allInformation[][] = new String[20][8];
+            int j=-1;
+            startMenu(allInformation,j);
+        }
     }
-    public static void main(String[] args) {
-        String allInformation[][] = new String[20][8];
-        startMenu(allInformation);
-    }
-}
+
 //Light bulb - LED 75W |
 // Expiry date: n/a |
 // Entry date: 05.05.2021 |
